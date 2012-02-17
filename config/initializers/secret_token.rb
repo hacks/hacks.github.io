daa2b68f197f3e0b72499d307d3f9e19e66d0384
Ali Ukani <ali.ukani@gmail.com>
2012-02-17 16:37:57 -0500

    I wish I could test this locally.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nuhacks::Application.config.secret_token = '0c2d84f5673a5bf9d1929a01cbbea1e3d90f7b4227728ee1417fcf7a6de3fc86ef300edef76d9e813da55fe4ee75710d6a500398fe333a04035a516ac4af0da8'
