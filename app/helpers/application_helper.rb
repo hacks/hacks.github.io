0c4cbe3d9763abf486eeac7449af520a525a3f4c
Ali Ukani <ali.ukani@gmail.com>
2012-02-17 20:30:37 -0500

    Added a helper method so I can get at Redcarpet's markdown parsing goodness.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def markdown_to_html(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                       :autolink => true,
+                                       :space_after_headers => true)
+    raw markdown.render(text)
+  end
+
 end
