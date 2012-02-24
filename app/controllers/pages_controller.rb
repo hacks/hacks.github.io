86b2d8331a4f55b6a27e7eb55202286538fcb160
Ali Ukani <ali.ukani@gmail.com>
2012-02-23 22:44:12 -0500

    Able to access upcoming events and next event.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def index
+    @next_events = Event.where("end_time >= ?", Date.today).order("start_time asc").limit(5)
+    @next_event = @next_events[1]
   end
 end
