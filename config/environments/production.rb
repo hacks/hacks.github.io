c7d164ff6b5db640ebcdb210a9a313530393aae2
Ali Ukani <ali.ukani@gmail.com>
2012-03-29 10:56:28 -0400

    Use config vars for admin user and pass in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ NUHacks::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  NUHacks::Admin[:username] = ENV['ADMIN_USER']
+  NUHacks::Admin[:password] = ENV['ADMIN_PASS']
 end
