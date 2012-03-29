fc9c8a547f4a44ef4c34ec0202c7b1f32ee2c3d0
Ali Ukani <ali.ukani@gmail.com>
2012-03-29 04:15:47 -0400

    Changed 'Nuhacks' to 'NUHacks'

--- Rakefile
+++ Rakefile
@@ -4,4 +4,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-Nuhacks::Application.load_tasks
+NUHacks::Application.load_tasks
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@ NUHacks::Application.config.session_store :cookie_store, key: '_nuhacks_session'
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Nuhacks::Application.config.session_store :active_record_store
+# NUHacks::Application.config.session_store :active_record_store
