782badfee0ff1d56d892ac90ccb569f9ff9f63d6
Ali Ukani <ali.ukani@gmail.com>
2012-03-29 04:00:43 -0400

    Moved admin username and password to environment variables.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,3 @@
 class AdminController < ApplicationController
-  http_basic_authenticate_with :name => "frodo", :password => "thering"
+  http_basic_authenticate_with name: NUHacks::Admin[:username], password: NUHacks::Admin[:password]
 end
