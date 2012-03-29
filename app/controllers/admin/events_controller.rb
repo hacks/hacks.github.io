ff160b2ba727bbf48b3e713d53f6610f3fbf907c
Ali Ukani <ali.ukani@gmail.com>
2012-03-29 03:06:10 -0400

    Admin panel working

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -44,7 +44,7 @@ class Admin::EventsController < AdminController
 
     respond_to do |format|
       if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.html { redirect_to [:admin, @event], notice: 'Event was successfully created.' }
         format.json { render json: @event, status: :created, location: @event }
       else
         format.html { render action: "new" }
@@ -60,7 +60,7 @@ class Admin::EventsController < AdminController
 
     respond_to do |format|
       if @event.update_attributes(params[:event])
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+        format.html { redirect_to [:admin, @event], notice: 'Event was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -76,7 +76,7 @@ class Admin::EventsController < AdminController
     @event.destroy
 
     respond_to do |format|
-      format.html { redirect_to events_url }
+      format.html { redirect_to admin_events_url }
       format.json { head :no_content }
     end
   end
