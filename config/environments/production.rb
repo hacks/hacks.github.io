43c4cdaa989c2e66384a4343f82571a845aa125d
Nathan Lilienthal <nathanlil13@me.com>
2013-02-17 15:12:11 -0500

    wtf assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,8 @@ Nuhacks::Application.configure do
   # Devise recommends this for Heroku deployment
   config.assets.initialize_on_precompile = false
 
+  config.assets.precompile = ['*.js', '*.css']
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
