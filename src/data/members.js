f2ba466dd81426fbc75b0927b69408d4eb5678e0
Jennings Zhang <jenni_zh@protonmail.com>
2020-06-26 00:52:03 -0400

    Jennings edited and linted his information on the site.

--- src/data/members.js
+++ src/data/members.js
@@ -1,79 +1,76 @@
-const MEMBERS = [
-    {
-        name: 'Jennings Zhang',
-        school: 'KCCS + COS',
-        year: '2022',
-        hasImage: true,
-        links: {
-            gitHub: 'jennydaman',
-            website: 'https://jennydaman.gitlab.io/',
-            email: 'jz@nuhacks.io',
-        },
-        ccs: 'jenni',
-        interests: [
-            'Computational Medicine',
-            'Human gut microbiome',
-            'Neuroinformatics',
-            'Education (fundies bad)',
-            'Cybersecurity',
-            '{{buzzword[8]}}',
-            'Philosophy and Debate',
-            'Chinese tea'
-        ]
+export default [
+  {
+    name: 'Jennings Zhang',
+    school: 'CCIS + COS',
+    year: '2022',
+    hasImage: true,
+    links: {
+      gitHub: 'jennydaman',
+      website: 'https://jennydaman.gitlab.io/',
+      email: 'jz@nuhacks.io',
     },
-    {
-        name: 'Edward Li',
-        school: 'KCCS',
-        year: '2021',
-        hasImage: true,
-        links: {
-            gitHub: 'NEUDitao',
-            website: 'https://eddyli.dev/',
-            email: 'li.edwa@northeastern.edu',
-        },
-        ccs: 'ditao1',
-        interests: [
-            'anything, literally anything.',
-            'I love musicals',
-            'basketball'
-        ]
+    ccs: 'jenni',
+    interests: [
+      'Biomedicine',
+      'Neuroinformatics',
+      'Education (fundies bad)',
+      'Cybersecurity',
+      '{{buzzword[8]}}',
+      'Philosophy',
+      'Smash Bros Ultimate'
+    ]
+  },
+  {
+    name: 'Edward Li',
+    school: 'KCCS',
+    year: '2021',
+    hasImage: true,
+    links: {
+      gitHub: 'NEUDitao',
+      website: 'https://eddyli.dev/',
+      email: 'li.edwa@northeastern.edu',
     },
-    {
-        name: 'Brandon Liang',
-        school: 'KCCS',
-        year: '2023',
-        hasImage: true,
-        links: {
-            gitHub: '54skyxenon',
-            website: 'https://brandonliang.com/',
-            email: 'liang.bra@northeastern.edu',
-        },
-        ccs: 'bwliang2001',
-        interests: [
-            'Anime',
-            'Hackathons',
-            'Travel',
-            'YGO'
-        ]
+    ccs: 'ditao1',
+    interests: [
+      'anything, literally anything.',
+      'I love musicals',
+      'basketball'
+    ]
+  },
+  {
+    name: 'Brandon Liang',
+    school: 'KCCS',
+    year: '2023',
+    hasImage: true,
+    links: {
+      gitHub: '54skyxenon',
+      website: 'https://brandonliang.com/',
+      email: 'liang.bra@northeastern.edu',
     },
-    {
-        name: 'Daniel Melcer',
-        school: 'KCCS',
-        year: '2021',
-        hasImage: true,
-        links: {
-            gitHub: 'dmelcer9',
-            website: 'https://brandonliang.com/',
-            email: 'melcer.d@northeastern.edu',
-        },
-        ccs: 'dmelcer9',
-        interests: [
-            'Hackathons',
-            'Robotics',
-            'TV shows',
-            "basically anything, though I don't really keep up with celebrities and such"
-        ]
-    }
+    ccs: 'bwliang2001',
+    interests: [
+      'Anime',
+      'Hackathons',
+      'Travel',
+      'YGO'
+    ]
+  },
+  {
+    name: 'Daniel Melcer',
+    school: 'KCCS',
+    year: '2021',
+    hasImage: true,
+    links: {
+      gitHub: 'dmelcer9',
+      website: 'https://brandonliang.com/',
+      email: 'melcer.d@northeastern.edu',
+    },
+    ccs: 'dmelcer9',
+    interests: [
+      'Hackathons',
+      'Robotics',
+      'TV shows',
+      "basically anything, though I don't really keep up with celebrities and such"
+    ]
+  }
 ];
-
-export default MEMBERS;
